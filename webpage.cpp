e4fb0eaad7b6efd549b38e5a2ea134231512937d
Larry Shaffer <larrys@dakotacarto.com>
2014-09-15 12:58:24 -0600

    Add license headers

--- webpage.cpp
+++ webpage.cpp
@@ -1,3 +1,22 @@
+/***************************************************************************
+    webpage.cpp  -  test app for PKI integration in QGIS
+                             -------------------
+    begin                : 2014-09-12
+    copyright            : (C) 2014 by Boundless Spatial, Inc.
+    web                  : http://boundlessgeo.com
+    author               : Larry Shaffer
+    email                : larrys at dakotacarto dot com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "webpage.h"
 #include "ui_webpage.h"
 
